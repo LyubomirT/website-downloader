f502f0b9048bdfeb1513229a9c236d87d93eaed0
Lyubomir <scratchwix@gmail.com>
2023-04-05 11:22:35 +0300

    3 days break

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import wx
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
 import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
 
-def download_website(url, folder_path):
+def download_website(url, folder_path, download_all=False):
     # Download website HTML file
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -31,65 +33,80 @@ def download_website(url, folder_path):
         with open(filename, 'wb') as f:
             f.write(response.content)
 
+    # Follow internal links
+    if download_all:
+        internal_links = []
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href is not None and 'http' not in href:
+                internal_links.append(urljoin(url, href))
 
-class DownloadFrame(wx.Frame):
+        for link in internal_links:
+            page_folder = os.path.join(folder_path, urlparse(link).path.strip('/'))
+            os.makedirs(page_folder, exist_ok=True)
+            download_website(link, page_folder, download_all=False)
+
+
+
+class DownloadFrame(tk.Frame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, title="Website Downloader")
+        tk.Frame.__init__(self, parent)
 
-        panel = wx.Panel(self)
+        self.url_label = ttk.Label(self, text="Website URL:")
+        self.url_text = ttk.Entry(self)
+        self.folder_label = ttk.Label(self, text="Save to folder:")
+        self.folder_text = ttk.Entry(self)
+        self.folder_button = ttk.Button(self, text="Browse", command=self.on_folder_button)
+        self.download_button = ttk.Button(self, text="Download", command=self.on_download_button)
+        self.check_var = tk.BooleanVar(value=False)  # initialize checkbox to unchecked
+        self.check_button = ttk.Checkbutton(self, text="Download entire website", variable=self.check_var)
+        self.status_label = ttk.Label(self, text="")
 
-        url_label = wx.StaticText(panel, label="Website URL:")
-        self.url_text = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER)
-        folder_label = wx.StaticText(panel, label="Save to folder:")
-        self.folder_text = wx.TextCtrl(panel)
-        folder_button = wx.Button(panel, label="Browse")
-        self.download_button = wx.Button(panel, label="Download")
-        self.status_label = wx.StaticText(panel, label="")
+        self.url_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.url_text.grid(row=0, column=1, padx=5, pady=5, sticky="we")
+        self.folder_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        self.folder_text.grid(row=1, column=1, padx=5, pady=5, sticky="we")
+        self.folder_button.grid(row=1, column=2, padx=5, pady=5, sticky="e")
+        self.check_button.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        self.download_button.grid(row=2, column=1, padx=5, pady=5, sticky="we")
+        self.status_label.grid(row=3, column=0, columnspan=3, padx=5, pady=5, sticky="w")
 
-        self.Bind(wx.EVT_BUTTON, self.on_folder_button, folder_button)
-        self.Bind(wx.EVT_BUTTON, self.on_download_button, self.download_button)
-        self.Bind(wx.EVT_TEXT_ENTER, self.on_download_button, self.url_text)
+    def on_folder_button(self):
+        folder_path = filedialog.askdirectory()
+        if folder_path:
+            self.folder_text.delete(0, tk.END)
+            self.folder_text.insert(0, folder_path)
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(url_label, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.url_text, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(folder_label, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.folder_text, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(folder_button, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.download_button, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.status_label, 0, wx.EXPAND | wx.ALL, 5)
-
-        panel.SetSizer(sizer)
-        self.Show()
-
-    def on_folder_button(self, event):
-        dialog = wx.DirDialog(self, "Choose a directory:", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
-        if dialog.ShowModal() == wx.ID_OK:
-            self.folder_text.SetValue(dialog.GetPath())
-        dialog.Destroy()
-
-    def on_download_button(self, event):
-        url = self.url_text.GetValue()
-        folder_path = self.folder_text.GetValue()
+    def on_download_button(self):
+        url = self.url_text.get()
+        folder_path = self.folder_text.get()
         if not url:
-            self.status_label.SetLabel("Please enter a website URL.")
+            self.status_label.config(text="Please enter a website URL.")
             return
         if not folder_path:
-            self.status_label.SetLabel("Please choose a folder to save the website.")
+            self.status_label.config(text="Please choose a folder to save the website.")
             return
 
-        self.download_button.Disable()
-        self.status_label.SetLabel("Downloading website...")
+        self.download_button.config(state="disabled")
+        self.status_label.config(text="Downloading website...")
         try:
-            download_website(url, folder_path)
-            self.status_label.SetLabel("Website downloaded successfully!")
+            if self.check_var.get():
+                download_website(url, folder_path)
+            else:
+                page_folder = os.path.join(folder_path, urlparse(url).path.strip('/'))
+                os.makedirs(page_folder, exist_ok=True)
+                page_url = urljoin(url, urlparse(url).path)  # Use the base URL of the page
+                download_website(page_url, page_folder)  # Download only the specified page
+            self.status_label.config(text="Website downloaded successfully!")
         except Exception as e:
-            self.status_label.SetLabel("An error occurred while downloading the website.")
+            self.status_label.config(text="An error occurred while downloading the website.")
             print(e)
-        self.download_button.Enable()
+        self.download_button.config(state="normal")
+
 
 
 if __name__ == "__main__":
-    app = wx.App()
-    frame = DownloadFrame(None)
-    app.MainLoop()
+    root = tk.Tk()
+    root.title("Download website")
+    DownloadFrame(root).pack(expand=True, fill="both")
+    root.mainloop()
